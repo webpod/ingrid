e55438e50a8b64b9cf4e246396f94d84239b5d8e
Anton Golub <antongolub@antongolub.com>
2024-03-22 16:46:07 +0300

    chore: add `parseLine` util

---
+++ src/main/ts/ingrid.ts
@@ -0,0 +1,83 @@
+import process from 'node:process'
+
+export type TIngridResponse = {
+  headers: string[]
+  rows: string[][]
+}
+
+export type TIngridParseOpts = Partial<{
+  format: 'unix' | 'win'
+}>
+
+export type TIngridParse = (input: string) => TIngridResponse | undefined
+
+const isWin = process.platform === 'win32'
+const EOL = /\r?\n|\r|\n/
+
+type TLineDigest = {
+  spaces: number[],
+  words: {s: number, e: number, w: string}[]
+}
+
+export const parseLine = <T>(line: string): TLineDigest => {
+  const result: TLineDigest = {
+    spaces: [],
+    words: []
+  }
+  const capture = () => {
+    if (word) {
+      result.words.push({
+        s,
+        e: s + word.length,
+        w: word
+      })
+      word = ''
+    }
+  }
+  let bb: string | undefined
+  let word = ''
+  let s = 0
+  for (const i in [...line]) {
+    const prev: string = line[+i - 1]
+    const char = line[i]
+    if (bb) {
+      word += char
+      if (char === bb && prev !== '\\') {
+        bb = undefined
+      }
+      continue
+    }
+    if (char === ' ') {
+      result.spaces.push(+i)
+      capture()
+      continue
+    }
+    s = +i
+    if (char === '"' || char === "'") bb = char
+    word += char
+  }
+
+  capture()
+
+  return result
+}
+
+export const parseLines = (input: string): TLineDigest[] =>
+  input.split(EOL).map(parseLine)
+
+export const parseUnixGrid = (input: string): TIngridResponse | undefined => {}
+
+export const parseWinGrid = (input: string): TIngridResponse | undefined => {}
+
+const parsers = {
+  unix: parseUnixGrid,
+  win: parseWinGrid
+}
+
+export const parse: TIngridParse = (input, {format = isWin ? 'win' : 'unix'}: TIngridParseOpts = {}) => {
+  const parser = parsers[format]
+  if (!parser) throw new Error(`unsupported format: ${format}`)
+
+  return parser(input)
+}
+
---
+++ src/test/ts/ingrid.test.ts
@@ -0,0 +1,62 @@
+import * as assert from 'node:assert'
+import { describe, it } from 'node:test'
+import * as path from 'node:path'
+import * as fs from 'node:fs/promises'
+import { parseUnixGrid, parseLine, parseLines } from '../../main/ts/ingrid.ts'
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const fixtures = path.resolve(__dirname, '../fixtures')
+
+// describe('parseUnixGrid()', () => {
+//   it('parses ps output', async () => {
+//     const output = await fs.readFile(path.resolve(fixtures, 'ps-unix-output.txt'), 'utf8')
+//     const result = parseUnixGrid(output)
+//     console.log(result)
+//     // assert.equal(foo(), undefined)
+//   })
+// })
+
+describe('parseLine()', () => {
+  it('extracts words ans spaces respecting quotes', () => {
+    const line = `foo bar "baz qux"   'a b "c"'`
+    const result = parseLine(line)
+    assert.deepEqual(result, {
+      spaces: [3, 7, 17, 18, 19],
+      words: [
+        {s: 2, e: 5, w: 'foo'},
+        {s: 6, e: 9, w: 'bar'},
+        {s: 8, e: 17, w: '"baz qux"'},
+        {s: 20, e: 29, w: `'a b "c"'`}
+      ]
+    })
+  })
+
+  it ('multiline', () => {
+    const lines = `foo bar "baz qux"
+'a b "c"' d   e
+`
+    const result = parseLines(lines)
+    assert.deepEqual(result, [
+      {
+        spaces: [3, 7],
+        words: [
+          {s: 2, e: 5, w: "foo"},
+          {s: 6, e: 9, w: "bar"},
+          {s: 8, e: 17, w: '"baz qux"'}
+        ]
+      },
+      {
+        spaces: [9, 11, 12, 13],
+        words: [
+          {s: 0, e: 9, w: "'a b \"c\"'"},
+          {s: 10, e: 11, w: 'd'},
+          {s: 14, e: 15, w: 'e'}
+        ]
+      },
+      {
+        spaces: [],
+        words: []
+      }
+    ])
+  })
+})
