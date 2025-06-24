a0b964b83d78c5764b6ab942a73d2fa889936206
Anton Golub <antongolub@antongolub.com>
2025-06-24 18:28:34 +0300

    test: tweak up integration test

--- src/test/ts/integration.test.ts
+++ src/test/ts/integration.test.ts
@@ -1,45 +1,49 @@
 import process from 'node:process'
 import { EOL } from 'node:os'
-// import * as assert from 'node:assert'
+import * as assert from 'node:assert'
 import cp from 'node:child_process'
 import { describe, it } from 'node:test'
 import {
   parseUnixGrid,
   parseWinGrid
 } from '../../main/ts/ingrid.ts'
-// import path from 'node:path'
-// import * as fs from 'node:fs/promises'
 
 const IS_WIN = process.platform === 'win32'
 
-
 describe('integration', () => {
   IS_WIN && it('parse wmic output', () => {
-    const input = `wmic process get ProcessId,ParentProcessId,CommandLine ${EOL}`
+    const input = `wmic process get ProcessId,ParentProcessId,CommandLine${EOL}`
     const cmd = 'cmd'
     const extractWmic = (stdout: string): string =>
-      stdout.slice(stdout.indexOf('CommandLine'))
-
+      stdout.slice(stdout.indexOf(input) + input.length).trim()
     const res = cp.spawnSync(cmd, {
       input,
       encoding: 'utf-8'
-    }).stdout
+    }).stdout.trim()
+    const parsed = parseWinGrid(extractWmic(res))
+    const first = parsed[0]
+    console.log('first', first)
 
-    console.log('res', res)
-    console.log('parsed', parseWinGrid(extractWmic(res)))
+    assert.ok(parsed.length > 0)
+    assert.equal(typeof first.CommandLine[0], 'string')
+    assert.equal(typeof first.ParentProcessId[0], 'string')
   })
 
   !IS_WIN && it('parses ps output', () => {
     const cmd = 'ps'
     const args = ['-lx']
-
     const res = cp.spawnSync(cmd, args, {
       encoding: 'utf-8',
       windowsHide: true,
       maxBuffer: 1024 * 1024
-    }).stdout
+    }).stdout.trim()
+    const parsed = parseUnixGrid(res)
+    const first = parsed[0]
+    console.log('first', first)
 
-    console.log('parsed', parseUnixGrid(res))
+    assert.ok(parsed.length > 0)
+    assert.equal(typeof (first.CMD || first.COMMAND)[0], 'string')
+    assert.equal(typeof first.PID[0], 'string')
   })
 })
 
